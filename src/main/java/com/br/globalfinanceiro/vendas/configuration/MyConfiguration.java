2c43346cbbe877817adf5da106bd01f45f6f0347
Atarian-ByVoid <cgomespimentel7@gmail.com>
2023-03-08 10:37:13 -0300

    Configuração externalizada: Application properties

--- src/main/java/com/br/globalfinanceiro/vendas/configuration/MyConfiguration.java
+++ src/main/java/com/br/globalfinanceiro/vendas/configuration/MyConfiguration.java
@@ -1,16 +1,9 @@
 package com.br.globalfinanceiro.vendas.configuration;
 
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class MyConfiguration {
-
-    @Bean(name="applicationName")
-    public String applicationName(){
-        return "Sistema de vendas";
-
-    }
 
     
 }
