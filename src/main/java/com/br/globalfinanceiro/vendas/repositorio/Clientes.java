4c228451412324f8fcdbc2c665df491655f72970
Atarian-ByVoid <cgomespimentel7@gmail.com>
2023-03-09 18:07:12 -0300

    terminar

--- src/main/java/com/br/globalfinanceiro/vendas/repositorio/Clientes.java
+++ src/main/java/com/br/globalfinanceiro/vendas/repositorio/Clientes.java
@@ -17,16 +17,41 @@ public class Clientes {
 
     private static String INSERT="Insert into cliente (nome) values (?)";
     private static String SELECT_ALL="SELECT * FROM CLIENTE";
+    private static String UPDATE="update cliente set nome=? where id=?";
+    private static String DELETE="delete from cliente where nome id=?";
+ 
+
 
 
     @Autowired
     private JdbcTemplate jdbcTemplate;
-
+    //salva um cliente
     public Cliente salvar(Cliente cliente){
         jdbcTemplate.update(INSERT, new Object[]{cliente.getNome()} );
         return cliente;
         
     }
+    //atualiza um cliente
+    public Cliente atualizar (Cliente cliente){
+        jdbcTemplate.update(UPDATE, new Object[]{cliente.getNome(), cliente.getId()});
+        return cliente;
+    }
+
+    public void deletar(Cliente cliente){
+        deletar(cliente.getId());
+    }
+
+    public void deletar(Integer id){
+        jdbcTemplate.update(DELETE, new Object[]{id});
+
+    }
+
+    public List<Clientes> buscarPorNome(String nome){
+        return jdbcTemplate.query(SELECT_ALL, new RowMapper, null, null)
+    }
+
+
+    //lista todos os clientes
     public List<Cliente> obterTodos(){
         return jdbcTemplate.query(SELECT_ALL, new RowMapper<Cliente>(){
             @Override
